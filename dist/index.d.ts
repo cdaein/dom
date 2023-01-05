22e6da603dcd76bc97c85fb6c4655b51eb54ff52
user <3522003+cdaein@users.noreply.github.com>
2023-01-05 16:15:49 -0500

    build

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,4 +1,10 @@
 /**
+ * append child to parent. if parent is undefined, append child to body
+ * @param parent parent element or string selector (may be undefined)
+ * @param child child element or string selector
+ */
+export declare const appendChild: (parent: Element | string | undefined, child: Element | string) => void;
+/**
  * convert selector (string) to DOM Element. if already typeof Element, return as is.
  * @param selector
  * @returns
--- dist/index.js
+++ dist/index.js
@@ -1,6 +1,22 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.toDomElement = void 0;
+exports.toDomElement = exports.appendChild = void 0;
+/**
+ * append child to parent. if parent is undefined, append child to body
+ * @param parent parent element or string selector (may be undefined)
+ * @param child child element or string selector
+ */
+const appendChild = (parent, child) => {
+    child = (0, exports.toDomElement)(child);
+    if (parent !== undefined) {
+        parent = (0, exports.toDomElement)(parent);
+        parent.appendChild(child);
+    }
+    else {
+        document.body.appendChild(child);
+    }
+};
+exports.appendChild = appendChild;
 /**
  * convert selector (string) to DOM Element. if already typeof Element, return as is.
  * @param selector
