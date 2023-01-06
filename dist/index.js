29170d9361295dda027d5055bde2a20f9522d3c9
user <3522003+cdaein@users.noreply.github.com>
2023-01-06 13:54:40 -0500

    update to esm

--- dist/index.js
+++ dist/index.js
@@ -1,29 +1,25 @@
-"use strict";
 /**
  * append child to parent. if parent is undefined, append child to body
  * @param parent parent element or string selector (may be undefined)
  * @param child child element or string selector
  */
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.toHTMLElement = exports.toElement = exports.appendChild = void 0;
-const appendChild = (parent, child) => {
-    child = (0, exports.toElement)(child);
+export const appendChild = (parent, child) => {
+    child = toElement(child);
     if (parent !== undefined) {
-        parent = (0, exports.toElement)(parent);
+        parent = toElement(parent);
         parent.appendChild(child);
     }
     else {
         document.body.appendChild(child);
     }
 };
-exports.appendChild = appendChild;
 /**
  * convert selector (string) to Element. if already typeof Element, return as is.
  *
  * @param selector
  * @returns
  */
-const toElement = (selector) => {
+export const toElement = (selector) => {
     if (typeof selector === "string") {
         const element = document.querySelector(selector);
         if (!element) {
@@ -33,14 +29,13 @@ const toElement = (selector) => {
     }
     return selector;
 };
-exports.toElement = toElement;
 /**
  * convert selector (string) to HTMLElement. if already typeof HTMLElement, return as is.
  *
  * @param selector
  * @returns
  */
-const toHTMLElement = (selector) => {
+export const toHTMLElement = (selector) => {
     if (typeof selector === "string") {
         const element = document.querySelector(selector);
         if (!element) {
@@ -50,5 +45,4 @@ const toHTMLElement = (selector) => {
     }
     return selector;
 };
-exports.toHTMLElement = toHTMLElement;
 //# sourceMappingURL=index.js.map
